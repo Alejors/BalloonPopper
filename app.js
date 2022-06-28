1079dbc0798b4bd103f20728f18c4b51477dc3e3
alejo <alejoatria@gmail.com>
2022-06-28 13:09:14 -0400

    Se realiza juego para reventar globos

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-// we declare a new global variable containing an array that represents the ballons map
-// you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
+let ballonsMap = ['green', 'yellow', 'teal', 'purple', 
+'pink', 'orange', 'black', 'white','red', 'turquoise',
+'grey', 'lightblue', 'brown','green', 'yellow', 'teal', 
+'purple', 'blue', 'black', 'turquoise'];
+let reventados = 0;
 
-// poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
-    // set the color to null on the balloon position
+
+    ballonsMap[position] = null;
+    reventados++;
     render();
 }
 
 const render = () => {
     
-    // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        if(color !== null){
+        return `<div class="balloon active" onClick="popBalloon(${position})" style="background:${color}"></div>`; // <--- render each balloon
+        }
+        else {
+            return `<div class="balloon popped" style="background:${color}"></div>`;
+        }  
     });
-
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
+    let activeBalloons = ballons.length - reventados;
+    document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
-
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+    if(activeBalloons == 0) window.location.reload();
 }
 
-// this makes the "render" function trigger when the website starts existing
 window.onload = render();
